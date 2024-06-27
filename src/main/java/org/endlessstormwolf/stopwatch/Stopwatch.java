f61763f669552380a2488e71ab64b2fcb3a10e46
WolfBrotherPC <wolfbrother@goldshadow.org>
2024-06-27 18:13:24 -0500

    Initial commit

---
+++ src/main/java/org/endlessstormwolf/stopwatch/Stopwatch.java
@@ -0,0 +1,35 @@
+package org.endlessstormwolf.stopwatch;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Stopwatch implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("stopwatch");
+
+	public static KeyBinding stopStart;
+	public static KeyBinding reset;
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Stopwatch initializing!");
+		stopStart = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.stopwatch.stopstart", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_R, // The keycode of the key
+				"category.stopwatch.stopwatch"// The translation key of the keybinding's category.
+		));
+		reset = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.stopwatch.reset", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_G, // The keycode of the key
+				"category.stopwatch.stopwatch"// The translation key of the keybinding's category.
+		));
+	}
+}
---
+++ src/main/java/org/endlessstormwolf/stopwatch/mixin/InGameHudMixin.java
@@ -0,0 +1,138 @@
+package org.endlessstormwolf.stopwatch.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.ColorHelper;
+import org.endlessstormwolf.stopwatch.Stopwatch;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+    @Unique
+    private long timer = 0;
+    @Unique
+    private long timeStart = 0;
+
+    @Unique
+    private long timeOld = 0;
+
+    @Unique
+    private boolean timerActive = false;
+
+    @Unique
+    private boolean overlayActive = true;
+    @Shadow @Final
+    private MinecraftClient client;
+
+    @Shadow
+    private int scaledWidth;
+
+    @Shadow
+    private int scaledHeight;
+    @Shadow public TextRenderer getTextRenderer() {
+        return this.client.textRenderer;
+    }
+
+    @Shadow private void drawTextBackground(DrawContext context, TextRenderer textRenderer, int yOffset, int width, int color) {
+        int i = this.client.options.getTextBackgroundColor(0.0F);
+        if (i != 0) {
+            int j = -width / 2;
+            int var10001 = j - 2;
+            int var10002 = yOffset - 2;
+            int var10003 = j + width + 2;
+            Objects.requireNonNull(textRenderer);
+            context.fill(var10001, var10002, var10003, yOffset + 9 + 2, ColorHelper.Argb.mixColor(i, color));
+        }
+    }
+
+    @Unique
+    private String prepString(long num) {
+        String timeString;
+        if (num < 10) {
+            timeString = "0" + num;
+        } else {
+            timeString = String.valueOf(num);
+        }
+        return timeString;
+    }
+
+    @Inject(method = "render", at = @At("TAIL"))
+    public void render(DrawContext context, float tickDelta, CallbackInfo ci) {
+        float scale = (float) scaledWidth/320;
+        if (Stopwatch.reset.wasPressed()) {
+            if (!overlayActive) {
+                overlayActive = true;
+            } else {
+                if (timer == 0) {
+                    overlayActive = false;
+                }
+            }
+            if (timer != 0) {
+                client.player.sendMessage(Text.literal("Timer reset!"), false);
+            }
+            timer = 0;
+            timeOld = 0;
+            timerActive = false;
+        }
+        if (overlayActive) {
+            long timeCurrent = System.nanoTime();
+            if (Stopwatch.stopStart.wasPressed()) {
+                if (timerActive) {
+                    timerActive = false;
+                    client.player.sendMessage(Text.literal("Timer stopped!"), false);
+                } else {
+                    timerActive = true;
+                    timeStart = System.nanoTime();
+                    timeCurrent = System.nanoTime();
+                    timeOld = timer;
+                    client.player.sendMessage(Text.literal("Timer started!"), false);
+                }
+            }
+            if (timerActive) {
+                timer = timeOld + (timeCurrent - timeStart);
+            }
+            String time = getTime();
+            TextRenderer textRenderer = this.getTextRenderer();
+            int m = textRenderer.getWidth(time);
+            context.getMatrices().push();
+            context.getMatrices().translate((float) scaledWidth - (m + 5), (float) scaledHeight - 5, 0.0F);
+            RenderSystem.enableBlend();
+            context.getMatrices().push();
+            context.getMatrices().scale(scale, scale, scale);
+            this.drawTextBackground(context, textRenderer, -10, m, 16777215);
+            context.drawTextWithShadow(textRenderer, time, -m / 2, -10, 16777215);
+            context.getMatrices().pop();
+            RenderSystem.disableBlend();
+            context.getMatrices().pop();
+        }
+    }
+
+    @Unique
+    private String getTime() {
+        long milliseconds = (Math.round(timer/1E6)) % 1000;
+        long seconds = (Math.round(timer / 1E9)) % 60;
+        long minutes = Math.round(Math.floor(((timer / 1E9)/60) % 60));
+        long hours = Math.round(Math.floor((timer / 1E9)/(60*60)));
+        String millisecondsString;
+        if (milliseconds < 10) {
+            millisecondsString = "00" + milliseconds;
+        } else if (milliseconds < 100) {
+            millisecondsString = "0" + milliseconds;
+        } else {
+            millisecondsString = String.valueOf(milliseconds);
+        }
+        return prepString(hours) + ":" + prepString(minutes) + ":" + prepString(seconds) + ":" + millisecondsString;
+    }
+}
